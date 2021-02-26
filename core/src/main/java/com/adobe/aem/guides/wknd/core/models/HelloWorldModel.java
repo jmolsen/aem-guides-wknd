0cad8f9940da3f1442736777fe8b38797f8428e3
Jeff Molsen <jeff.molsen@gmail.com>
2021-02-26 16:09:46 -0600

    feat: Component Basics changes to HelloWorld component and clientlibs

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/HelloWorldModel.java
@@ -19,11 +19,12 @@ import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_T
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
@@ -32,12 +33,14 @@ import org.apache.sling.settings.SlingSettingsService;
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
 
+import java.util.Locale;
 import java.util.Optional;
 
-@Model(adaptables = Resource.class)
+@Model(adaptables = Resource.class,
+    defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class HelloWorldModel {
 
-    @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
+    @ValueMapValue(name=PROPERTY_RESOURCE_TYPE)
     @Default(values="No resourceType")
     protected String resourceType;
 
@@ -48,6 +51,12 @@ public class HelloWorldModel {
     @SlingObject
     private ResourceResolver resourceResolver;
 
+    @ValueMapValue
+    private String title;
+
+    @ValueMapValue
+    private String text;
+    
     private String message;
 
     @PostConstruct
@@ -63,6 +72,14 @@ public class HelloWorldModel {
             + "This is instance: " + settings.getSlingId() + "\n";
     }
 
+    public String getTitle() {
+        return StringUtils.isNotBlank(title) ? title : "Default value here!";
+    }
+
+    public String getText() {
+        return StringUtils.isNotBlank(text) ? text.toUpperCase() : null;
+    }
+    
     public String getMessage() {
         return message;
     }
